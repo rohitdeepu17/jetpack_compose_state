82377d87b8faed477cab042c87194e7e74aeb783
Rohit Singla <rohitdeepu17@gmail.com>
2023-08-23 15:00:00 +0530

    observable mutable list

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -2,6 +2,8 @@ package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -9,6 +11,9 @@ fun WellnessScreen(modifier: Modifier = Modifier){
     Column(modifier = modifier){
         //WaterCounter(modifier)
         StatefulCounter()
-        WellnessTasksList()
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = {task->list.remove(task)})
     }
 }
+
+private fun getWellnessTasks() = List(30) {i->WellnessTask(i, "Task # $i")}
--- app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
@@ -6,18 +6,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.lazy.items
 
-private fun getWellnessTasks() = List(30) {i->WellnessTask(i, "Task # $i")}
-
 @Composable
 fun WellnessTasksList(
-    modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTasks() }
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     LazyColumn(
         modifier = modifier
     ) {
-        items(list) { task ->
-            WellnessTaskItem(taskName = task.label)
+        items(items = list,
+        key = {task->task.id}) { task ->
+            WellnessTaskItem(taskName = task.label, onClose = {onCloseTask(task)})
         }
     }
 }
